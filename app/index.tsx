a69aa59d8512f3bcaf0564678c87dea6c062efb9
Gus Tonin <gstonin95@gmail.com>
2024-11-16 15:40:21 +0000

    clean up project & add basic navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+import Colors from "./constants/Colors";
+import { Pressable } from "react-native-gesture-handler";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>This is the best flag guessing game ever made</Text>
+    <View style={styles.pageContainer}>
+      <Text>Welcome to the best flag game ever made ?</Text>
+      <Pressable style={styles.btn} onPress={() => router.replace({ pathname: "/(tabs)" })}>
+        <Text>Start</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  pageContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 16,
+  },
+  btn: {
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    backgroundColor: Colors.light.btn,
+    borderRadius: 8,
+  },
+});
